b4c42fd7e5c8a8dfac5d770691e7430fd4a646a6
Shay <shay2442@yahoo.com>
2022-10-12 18:12:23 -0500

    Added onClick for likes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+const [time, setTime] = useState(0)
+const [timerOn, setTimerOn] = useState(false)
+const [likes, setLikes] = useState(0)
+
+useEffect(() => {
+  let interval = null
+  if(timerOn) {
+    interval = setInterval(() => {
+      setTime(prevTime => prevTime + 10)
+    },10)
+  }else {
+    clearInterval(interval)
+  }
+  return () => clearInterval(interval)
+}, [timerOn])
+
+
+ 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>{time}</div>
+    <button onClick={() => setTimerOn(true)}>start</button>
+    <button onClick={() => setTimerOn(false)}>stop</button>
+    <button onClick={() => setTime(0)}>reset</button>
+    <div>{likes}</div>
+    <button onClick={() => setLikes(likes + 1)}>likes</button>
+
+ 
+
+   
+</div>
   );
 }
 
